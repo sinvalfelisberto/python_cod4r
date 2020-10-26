c39f14306988d01482d7d3b7e72cd9f0a885e526
Sinval Felisbert <sinvalfelisberto@gmail.com>
2020-10-26 06:40:27 -0300

    operadores aritméticos

--- fundamentos/001primeiro_programa.py
+++ fundamentos/001primeiro_programa.py
@@ -29,7 +29,22 @@ você pode utilizar aspas
 duplas ou aspas simpres!
 """
 
-# para explicar algo no código, utilizar uma linha acima, antes do que se quer explicar
+# para explicar algo no código,
+# utilizar uma linha acima,
+# antes do que se quer explicar
 
 # variavel do salário
 salario = 1045.00
+
+# Operadores Aritméticos
+'''
+Operadores binários
+'''
+print(2 + 3)
+print(4 - 7)
+print(2 * 5.3)
+print(9.4 / 3)
+# divisão com resultado apenas inteiro.
+print(9.4 // 3)
+print(2 ** 8)
+print(10 % 3)
