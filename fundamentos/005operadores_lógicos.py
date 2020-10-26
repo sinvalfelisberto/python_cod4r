39c5cd1013b85b3d5643f11bf6a9421f4194ad60
Sinval Felisbert <sinvalfelisberto@gmail.com>
2020-10-26 10:38:44 -0300

    dando prosseguimento no estudo

---
+++ fundamentos/005operadores_lógicos.py
@@ -0,0 +1,28 @@
+print(True or False)
+print(7 != 3 and 2 > 3)
+# XOR - ou exclusisvo
+print(True != False)
+print(False != True)
+print(True != True)
+print(False != False)
+
+# Operadores bit-a-bit
+'''
+&, | e ^
+operam no valor binário de um
+dado.
+'''
+
+# exemplo
+saldo = 1000
+salario = 4000
+despesas = 2700
+
+saldo_positivo = saldo > 0
+despesas_controladas = salario - despesas >= 0.2 * salario
+
+meta = saldo_positivo and despesas_controladas
+if meta != False:
+    print("Meta batida")
+else:
+    print("Meta não batida")
