4b2511dc641123bb67ec92a4a5cfa2dc64c90c23
Sinval Felisbert <sinvalfelisberto@gmail.com>
2020-10-26 05:55:15 -0300

    fundamentos do python

---
+++ fundamentos/001primeiro_programa.py
@@ -0,0 +1,35 @@
+# Fundamentos
+# Primeiros Exemplos
+
+print('Primeiro Programa')
+
+# Tipos Básicos
+print(True)
+print(False)
+print(2.2 + 1)
+print('Aqui eu falo minha língua')
+print("aqui também")
+print('Você é ' + 3 * 'muito ' + 'legal')
+# print(3 + '3') -> ambiguidade
+print([1, 2, 3])
+print({'nome': 'Pedro', 'idade': 24})
+print(None)
+
+# Variáveis
+# Dinamicamente tipado.
+
+a = 10
+b = 5.2
+
+# Comentários no Código
+# de uma linha
+"""
+De múltiplas linhas
+você pode utilizar aspas
+duplas ou aspas simpres!
+"""
+
+# para explicar algo no código, utilizar uma linha acima, antes do que se quer explicar
+
+# variavel do salário
+salario = 1045.00
--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,11 @@
+a = 3
+b = [1,  2,    3]
+dicionario = {'nome': 'Sinval', 'idade': 38}
+
+for itens in b:
+    print(itens)
+
+
 print('Hello, World!')
+
+print(dicionario['nome'])
